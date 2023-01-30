e16567edaa475cdef739a5c51cfe7dc87ec8ce68
AyseGun <aishagun2015@gmail.com>
2023-01-30 12:57:27 +0200

    task 5 yapildi

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -5,6 +5,8 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("Ogrenciden merhaba");
         System.out.println("ders basladi");
+
+        System.out.println("merhabalar herkese");
     }
 
 }
